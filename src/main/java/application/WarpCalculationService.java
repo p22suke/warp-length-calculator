30c6c7c2b916136444920436c1745f8e597b74c4
siina.P22suke <liina@kolkaplika.ee>
2026-02-28 13:20:36 +0200

    Added service layer

--- src/main/java/application/WarpCalculationService.java
+++ src/main/java/application/WarpCalculationService.java
@@ -1,10 +1,19 @@
 package main.java.application;
 
-import main.java.domain.Item;
 import main.java.domain.WarpLengthCalculator;
+import main.java.domain.Item;
 
 import java.util.List;
 
 public class WarpCalculationService {
+ private final WarpLengthCalculator calculator;
+ // konstruktor
+ public WarpCalculationService(){
+    this.calculator = new WarpLengthCalculator();
+ }
+
+ public double calculate(List<Item> items, double lossPercentage) {
+    return calculator.calculateWarpLength(items, lossPercentage);
+ }
 
 }
